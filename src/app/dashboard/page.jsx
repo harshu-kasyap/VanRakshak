b4a0d07cffe5b7c8b42253ef9c43a89bc1325b7b
DevyManish <121728309+DevyManish@users.noreply.github.com>
2025-03-30 03:54:14 +0530

    feat: add dashborad

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,143 @@
-import React from 'react'
+"use client"
+import { AreaGraph } from '@/components/charts/area-graph';
+import { BarGraph } from '@/components/charts/bar-graph';
+import { PieGraph } from '@/components/charts/pie-graph';
+import { CalendarDateRangePicker } from '@/components/date-range-picker';
+import PageContainer from '@/components/page-container';
+import { RecentSales } from '@/components/recent-sales';
+import { Button } from '@/components/ui/button';
+import Sidebar from '@/components/sidebar';
+// import Header from '@/components/header';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Siren, Swords } from "lucide-react";
 
-const page = () => {
-  return (
-    <div className='w-full items-center justify-centers'>
-      dashboard
-    </div>
-  )
+export default function Page() {
+  return (<div className="flex">
+    <Sidebar />
+    <main className="w-full flex-1 overflow-hidden">
+      {/* <Header /> */}
+      <PageContainer scrollable={true}>
+        <div className="space-y-2">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-2xl font-bold tracking-tight">
+              Admin Dashboard
+            </h2>
+            <div className="hidden items-center space-x-2 md:flex">
+              <CalendarDateRangePicker />
+              <Button>Download</Button>
+            </div>
+          </div>
+          <Tabs defaultValue="overview" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="analytics" disabled>
+                Analytics
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="overview" className="space-y-4">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                      Total Incidents
+                    </CardTitle>
+                    <Siren size={15} color='#AAA5A2' />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">53</div>
+                    <p className="text-xs text-muted-foreground">
+                      +20.1% from last month
+                    </p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                      Incident Resolved
+                    </CardTitle>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-4 w-4 text-muted-foreground"
+                    >
+                      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                      <circle cx="9" cy="7" r="4" />
+                      <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                    </svg>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">+18</div>
+                    <p className="text-xs text-muted-foreground">
+                      +30.1% from last month
+                    </p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Illegal Activites</CardTitle>
+                    <Swords size={15} color='#AAA5A2' />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">5</div>
+                    <p className="text-xs text-muted-foreground">
+                      -9% from last month
+                    </p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                      Current Incident
+                    </CardTitle>
+                    <Siren size={15} color='#AAA5A2' />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">+3</div>
+                    <p className="text-xs text-muted-foreground">
+                      +201 since last hour
+                    </p>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7">
+                <div className="col-span-4">
+                  <AreaGraph />
+                </div>
+                <Card className="col-span-4 md:col-span-3">
+                  <CardHeader>
+                    <CardTitle>Recent Transactions</CardTitle>
+                    <CardDescription>
+                      Fund Raisers are on 12% surplus in transactions.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <RecentSales />
+                  </CardContent>
+                </Card>
+                <div className="col-span-4">
+                  <AreaGraph />
+                </div>
+                <div className="col-span-4 md:col-span-3">
+                  <PieGraph />
+                </div>
+              </div>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </PageContainer>
+    </main>
+  </div>
+  );
 }
-
-export default page
